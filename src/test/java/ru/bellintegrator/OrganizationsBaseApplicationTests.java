a79eeff238d2200daedc9ce47bc4c2533684d6c6
Ajulay <11v09v@gmail.com>
2018-12-09 21:00:39 +0300

    Улучшены интеграционные тесты.

--- src/test/java/ru/bellintegrator/OrganizationsBaseApplicationTests.java
+++ src/test/java/ru/bellintegrator/OrganizationsBaseApplicationTests.java
@@ -6,7 +6,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
@@ -34,10 +38,8 @@ public class OrganizationsBaseApplicationTests {
 
         final String baseUrl = "http://localhost:" + randomServerPort + "/api/countries";
         URI uri = new URI(baseUrl);
-
         Map<String, String> parts = new HashMap<>();
         ResponseEntity<String> result = restTemplate.postForEntity(uri, parts, String.class);
-
         Assert.assertEquals(200, result.getStatusCodeValue());
         Assert.assertEquals(true, result.getBody().contains("Россия"));
 
@@ -48,11 +50,10 @@ public class OrganizationsBaseApplicationTests {
      * */
     @Test
     public void docTypeAccessTest() throws URISyntaxException {
+
         final String baseUrl = "http://localhost:" + randomServerPort + "/api/docs";
         URI uri = new URI(baseUrl);
-
         ResponseEntity<String> result = restTemplate.postForEntity(uri, null, String.class);
-
         Assert.assertEquals(200, result.getStatusCodeValue());
         Assert.assertEquals(true, result.getBody().contains("21"));
 
@@ -63,15 +64,13 @@ public class OrganizationsBaseApplicationTests {
      * */
     @Test
     public void organizationTest() throws URISyntaxException {
+
         final String baseUrl = "http://localhost:" + randomServerPort + "/api/organization/1";
         URI uri = new URI(baseUrl);
-
         RestTemplate restTemplate = new RestTemplate();
-
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<String> entity = new HttpEntity<>("params", headers);
-
         ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
         Assert.assertTrue(result.getBody().contains("MTS"));
     }
